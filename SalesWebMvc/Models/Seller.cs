0b686685c536c781737c54dfa2497a4bbc1eb0a3
FernandoDebrie <fernandodebrie@gmail.com>
2021-09-27 05:17:28 -0400

    Seller Service and SellerrController with method FindAll

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -14,7 +14,7 @@ namespace SalesWebMvc.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public DateTime BirthDay { get; set; }
+        public DateTime BirthDate { get; set; }
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
@@ -24,7 +24,7 @@ namespace SalesWebMvc.Models
             Id = id;
             Name = name;
             Email = email;
-            BirthDay = birthDay;
+            BirthDate = birthDay;
             BaseSalary = baseSalary;
             Department = department;
         }
