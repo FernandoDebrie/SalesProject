bffd04df62b0eb322309e9d40db3ff9b0a837faf
FernandoDebrie <fernandodebrie@gmail.com>
2021-09-27 03:28:19 -0400

    Other entities and second migrations

---
+++ SalesWebMvc/Models/Enums/SaleStatus.cs
@@ -0,0 +1,9 @@
+﻿namespace SalesWebMvc.Models.Enums
+{
+    public enum SaleStatus: int
+    {
+        pending = 0,
+        billing = 1,
+        cancelled = 2
+    }
+}
---
+++ SalesWebMvc/Models/Seller.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SalesWebMvc.Models
+{
+    public class Seller
+    {
+
+        public Seller()
+        {
+
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public DateTime BirthDay { get; set; }
+        public double BaseSalary { get; set; }
+        public Department Department { get; set; }
+        public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
+
+        public Seller(int id, string name, string email, DateTime birthDay, double baseSalary, Department department)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            BirthDay = birthDay;
+            BaseSalary = baseSalary;
+            Department = department;
+        }
+
+        public void AddSale (SalesRecord sale)
+        {
+            Sales.Add(sale);
+        }
+        public void RemoveSale(SalesRecord sale)
+        {
+            Sales.Remove(sale);
+        }
+        public double TotalSale(DateTime initial, DateTime final)
+        {
+            return Sales.Where(s => s.Date >= initial && s.Date <= final).Select(s => s.Amount).Sum();
+        }
+    }
+}
